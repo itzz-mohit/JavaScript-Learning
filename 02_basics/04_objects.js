c931122be851745c9275c2440aa087de75d801c5
Mohit <mohitbite@gmail.com>
2024-01-28 17:47:33 +0530

    arrays and objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,82 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Mohit"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Mohit",
+            lastname: "Kumar"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "mk@gmail.com"
+    },
+    {
+        id: 1,
+        email: "mk@gmail.com"
+    },
+    {
+        id: 1,
+        email: "mk@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "JavaScript",
+    price: "999",
+    courseInstructor: "mohit"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "mohit",
+//     "coursename": "JavaScript",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
